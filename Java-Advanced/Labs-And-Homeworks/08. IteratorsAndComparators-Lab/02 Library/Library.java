93f08c3b251bf18feabcac6d5240ffe1caeba23b
Stela Tsolova <stela.tsolova@gmail.com>
2021-11-28 21:58:16 +0200

    Added 08 Lab

---
+++ Java-Advanced/Labs-And-Homeworks/08. IteratorsAndComparators-Lab/02 Library/Library.java
@@ -0,0 +1,28 @@
+package Library_2;
+
+import java.util.Iterator;
+
+public class Library implements Iterable<Book> {
+    private Book[] books;
+
+    public Library(Book... books) {
+        this.books = books;
+    }
+
+    @Override
+    public Iterator<Book> iterator() {
+        return new Iterator<Book>() {
+            private int index = 0;
+
+            @Override
+            public boolean hasNext() {
+                return index < books.length;
+            }
+
+            @Override
+            public Book next() {
+                return books[index++];
+            }
+        };
+    }
+}
---
+++ Java-Advanced/Labs-And-Homeworks/08. IteratorsAndComparators-Lab/03 ComparableBook/Main.java
@@ -0,0 +1,17 @@
+package ComparableBook_3;
+
+public class Main {
+    public static void main(String[] args) {
+        Book bookOne = new Book("Animal Farm", 2003, "George Orwell");
+        Book bookTwo = new Book("The Documents in the Case", 1930, "Dorothy Sayers", "Robert Eustace");
+        Book bookThree = new Book("The Documents in the Case", 2002);
+
+        if (bookOne.compareTo(bookTwo) < 0) {
+            System.out.println(String.format("%s is before %s", bookOne.getTitle(), bookTwo.getTitle()));
+        } else if (bookOne.compareTo(bookThree) < 0) {
+            System.out.println(String.format("%s is before %s", bookTwo.getTitle(), bookThree.getTitle()));
+        } else {
+            System.out.println("Book are equal");
+        }
+    }
+}
