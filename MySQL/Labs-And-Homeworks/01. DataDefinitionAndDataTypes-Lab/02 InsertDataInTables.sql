3935e2dac5cc9d6cce67d06548b3b704fec55f08
Stela Tsolova <stela.tsolova@gmail.com>
2022-02-07 13:42:50 +0200

    README.md-Lab

---
+++ MySQL/Labs-And-Homeworks/01. DataDefinitionAndDataTypes-Lab/02 InsertDataInTables.sql
@@ -0,0 +1,8 @@
+INSERT INTO `employees` (`id`, `first_name`, `last_name`) 
+VALUES (1,'test1', 'test');
+
+INSERT INTO `employees` (`id`, `first_name`, `last_name`)
+VALUES (2, 'test2', 'testt');
+
+INSERT INTO `employees` (`first_name`, `last_name`)
+VALUES ('test3',  'testtt');
